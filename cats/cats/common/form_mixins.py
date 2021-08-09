a556ddb364afb5b42c762f988f9cce31d724e98d
HOOLIGANS 801 <kristiyankan4ev@abv.bg>
2021-08-09 10:45:35 +0300

    test

---
+++ cats/cats/common/form_mixins.py
@@ -0,0 +1,9 @@
+class BootStrapFormViewMixin:
+    def get_form(self, **kwargs):
+        form = super().get_form(**kwargs)
+        self.__apply_bootstrap_classes(form)
+        return form
+
+    def __apply_bootstrap_classes(self, form):
+        for (_, field) in form.fields.items():
+            field.widget.attrs = {'class': 'form-control'}
